05f66cd6aaa698e7f9871eaac32528f3852001ae
Isaac <isaacdura@gmail.com>
2016-10-03 14:23:14 +0200

    GameTest

---
+++ src/test/java/test/GameTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import com.leanroute.test.rock.paper.scissors_designupdate.Player;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Isaac
+ */
+public class GameTest {
+
+    public GameTest() {
+    }
+
+    @Test
+    void playRound() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    boolean isTie() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    Player getWinner() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    Player getLoser() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+}
