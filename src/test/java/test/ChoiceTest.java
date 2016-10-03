2e3f60b164d9abd672c0a244779d0f1427c118ce
Isaac <isaacdura@gmail.com>
2016-10-03 14:22:59 +0200

    ChoiceTest

---
+++ src/test/java/test/ChoiceTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Isaac
+ */
+public class ChoiceTest {
+    
+    public ChoiceTest() {
+    }
+    
+    @Test
+    boolean beats(Choice other) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    
+}
