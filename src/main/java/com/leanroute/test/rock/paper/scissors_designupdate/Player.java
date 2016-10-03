87d5cf3779c16e22471bae2dbb6826abe65763dd
Isaac <isaacdura@gmail.com>
2016-10-03 10:55:57 +0200

    Class Player, and their functionalities

---
+++ src/main/java/com/leanroute/test/rock/paper/scissors_designupdate/Player.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leanroute.test.rock.paper.scissors_designupdate;
+
+/**
+ *
+ * @author Isaac
+ *
+ * Defines the functionalities of a player
+ *
+ *
+ */
+final class Player {
+    /**
+     * The player makes a choice
+     * @param 
+     * @return Choice
+     * @throw
+     */
+
+    Choice choose() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+}
