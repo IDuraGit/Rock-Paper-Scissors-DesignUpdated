9250d1bf362fe1eeaf15107749573d6e3e16f01d
Isaac <isaacdura@gmail.com>
2016-10-03 14:23:33 +0200

    PlayerTest

---
+++ src/test/java/test/PlayerTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.leanroute.test.rock.paper.scissors_designupdate.*;
+
+/**
+ *
+ * @author Isaac
+ */
+public class PlayerTest {
+    
+    public PlayerTest() {
+    }
+    
+    @Test
+     Choice choose() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+    
+}
